892495aded8143df0aa2415a798b76a51b6fe9e9
Caroline Tan <38272602+carolinetan@users.noreply.github.com>
2018-08-10 13:20:36 -0400

    Initial module implementation

---
+++ src/main/java/udemy/course/springframeworkpetclinic/SpringFrameworkPetclinicApplication.java
@@ -0,0 +1,12 @@
+package udemy.course.springframeworkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkPetclinicApplication.class, args);
+    }
+}
