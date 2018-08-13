80e532bac57394b0f46c3f7e77c7e6b6c01c491c
Caroline Tan <emailctan@gmail.com>
2018-08-13 12:30:19 -0400

    [spring-framework-petclinic] implement setting of ID Property for map repo. Closes #16

--- pet-clinic-web/src/main/java/udemy/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/udemy/springframework/petclinic/bootstrap/DataLoader.java
@@ -25,13 +25,11 @@ public class DataLoader implements CommandLineRunner {
 
         // load test data here
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Daniel");
         owner1.setLastName("Achacon");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Neil");
         owner2.setLastName("Peters");
         ownerService.save(owner2);
@@ -39,13 +37,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded 2 owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Frankie");
         vet1.setLastName("Fazio");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Kyla");
         vet2.setLastName("Achacon");
         vetService.save(vet2);
