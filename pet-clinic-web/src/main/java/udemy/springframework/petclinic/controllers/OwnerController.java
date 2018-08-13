3629a9712e820ded61c8c47e5ebf24b5f68267b5
Caroline Tan <emailctan@gmail.com>
2018-08-13 11:35:32 -0400

    [spring-framework-petclinic] implement listing of owners. Closes #17

--- pet-clinic-web/src/main/java/udemy/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/udemy/springframework/petclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package udemy.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import udemy.springframework.petclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    //@Autowired --> Optional
+    //               In Spring 5, spring automatically does the autowiring
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/","/index", "/index.html"})
-    public String listOfOwners() {
+    public String listOfOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
