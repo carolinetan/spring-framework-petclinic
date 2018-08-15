ae832ffe9b94c514c7e65e950db9d1903c95e82e
Caroline Tan <emailctan@gmail.com>
2018-08-15 08:03:59 -0400

    [spring-framework-petclinic] Convert Owners to JPA Entities. Closes #25

--- pet-clinic-data/src/main/java/udemy/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/udemy/springframework/petclinic/model/Pet.java
@@ -1,12 +1,25 @@
 package udemy.springframework.petclinic.model;
 
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
-    private PetType type;
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
 
@@ -18,12 +31,12 @@ public class Pet extends BaseEntity {
         this.birthDate = birthDate;
     }
 
-    public PetType getType() {
-        return type;
+    public PetType getPetType() {
+        return petType;
     }
 
-    public void setType(PetType type) {
-        this.type = type;
+    public void setPetType(PetType petType) {
+        this.petType = petType;
     }
 
     public Owner getOwner() {
